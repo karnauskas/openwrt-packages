630b80f5767d709fabb92bb03e90518b460db6be
Marius Karnauskas <marius.karnauskas@gmail.com>
2021-10-31 02:45:17 +0300

    try build

--- luci/applications/luci-app-tailscale/luasrc/model/cbi/tailscale/main.lua
+++ luci/applications/luci-app-tailscale/luasrc/model/cbi/tailscale/main.lua
@@ -1,2 +1,12 @@
-m = Map("tailscale", translate("Tailscale"))
+m = Map("tailscale", translate("Tailscale"), translate("Tailscale VPN"))
+
+s = m:section(TypedSection, "main", "Main")
+s.anonymous = true
+
+dns = s:option(Flag, "dns", "accept DNS configuration from the admin panel")
+dns.rmempty = false
+
+exit = s:option(Flag, "exit", "offer to be an exit node for internet traffic for the tailnet")
+dns.rmempty = false
+
 return m
