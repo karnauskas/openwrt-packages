24902c1daf69f60e8532c5569fc0f2f3c8c2eac6
Marius Karnauskas <marius.karnauskas@gmail.com>
2021-10-31 03:01:54 +0300

    Not found

--- applications/luci-app-tailscale/luasrc/controller/tailscale.lua
+++ applications/luci-app-tailscale/luasrc/controller/tailscale.lua
@@ -1,7 +1,12 @@
 module("luci.controller.tailscale", package.seeall)
 
 function index()
-    page = entry({"admin", "vpn", "tailscale"}, alias("admin", "vpn", "tailscale", "main"), _("Tailscale"))
-    page.dependent = true
+    local page
+
+    if not nixio.fs.access("/etc/config/tailscale") then
+        nxfs.writefile("/etc/config/tailscale", "")
+    end
+
+    page = entry({"admin", "vpn", "tailscale"}, cbi("tailscale/main"), _("Tailscale"))
     page.acl_depends = { "luci-app-tailscale" }
 end
